b7b9b1bd81fd5513b44d6b0a9f7f9cfa4c17745d
christam27 <1155079774@link.cuhk.edu.hk>
2019-05-09 07:27:30 +0800

    update file

--- inject.js
+++ inject.js
@@ -1,1 +1,29 @@
+var count = 0;
+function onclick(){
+    var items = document.getElementById("items-stamp");
+    var alldelebtn = items.getElementsByTagName("button");
+
+    for(var i = 0; i < alldelebtn.length; i++){
+        alldelebtn[i].click();
+    }
+
+    if(count < 50){
+        onclick();
+    }
+}
+
+console.log("running improve youtube context script");
+var playlist = document.getElementById("playlist-actions");
+console.log(playlist);
+
+var delebtn = document.createElement("button");
+var btnid = document.createAttribute("id");
+btnid.value = "deleteplaylist";
+delebtn.setAttributeNode(btnid);
+delebtn.innerHTML = "Delete Playlist";
+delebtn.addEventListener("click",onclick);
+
+playlist.appendChild(delebtn);
+
+
 console.log("running improve youtube context script");
--- popup.js
+++ popup.js
@@ -19,7 +19,8 @@ xhr.onreadystatechange = function() {
 xhr.send();
 
 var videolist = [];
-for(var i = 0; i < 100; i++){
+var channels_link = [];
+for(var i = 0; i < channels_access.length; i++){
   if(i > 0){
     // console.log(channels_names[i]);
     // console.log(channels_access[i]);
@@ -35,6 +36,8 @@ for(var i = 0; i < 100; i++){
           // publisher
           var tempauthor = videos[j].getElementsByTagName("author");
           var tempauthorname = tempauthor[0].getElementsByTagName("name");
+          var tempauthoruri = tempauthor[0].getElementsByTagName("uri");
+          var authoruri = tempauthoruri[0].innerHTML;
           var author = tempauthorname[0].innerHTML;
           // console.log(tempauthorname[0].innerHTML);
           // title
@@ -53,6 +56,7 @@ for(var i = 0; i < 100; i++){
           // console.log(temptime[0].innerHTML);
           // push to array
           videolist.push([author,title,link,time]);
+          channels_link.push([author,authoruri]);
         }
         // console.log(videos)
       }
@@ -60,6 +64,7 @@ for(var i = 0; i < 100; i++){
     xhr2.send();
   }
 }
+console.log(channels_link);
 
 videolist.sort(sortfunction);
 console.log(videolist);
@@ -72,53 +77,338 @@ function sortfunction(a, b) {
   }
 }
 
-var show = document.getElementById("showjs");
-for(var i = 0; i < 15; i++){
-  var div = document.createElement("div");
-  var att = document.createAttribute("class");
-  att.value = "card";
-  div.setAttributeNode(att);
+var page = 1;
+// default
+clickpage(1);
 
-  var header = document.createElement("h5");
-  var att2 = document.createAttribute("class");
-  att2.value = "card-header";
-  header.setAttributeNode(att2);
-  header.innerHTML = videolist[i][0];
-  div.appendChild(header);
+function clickpage(e){
+  console.log("set page");
+  page = e;
+  console.log(page);
+  switch(page){
+    case 1:
+      // default
+      var show = document.getElementById("showjs");
+      show.innerHTML = "";
+      for(var i = 0; i < 15; i++){
+        var div = document.createElement("div");
+        var att = document.createAttribute("class");
+        att.value = "card";
+        div.setAttributeNode(att);
 
-  var divbody = document.createElement("div");
-  var att3 = document.createAttribute("class");
-  att3.value = "card-body";
-  divbody.setAttributeNode(att3);
+        var header = document.createElement("h5");
+        var att2 = document.createAttribute("class");
+        att2.value = "card-header";
+        header.setAttributeNode(att2);
+        header.innerHTML = videolist[i][0];
+        div.appendChild(header);
 
-  var header2 = document.createElement("h6");
-  var att3 = document.createAttribute("class");
-  att3.value = "card-title";
-  header2.setAttributeNode(att3);
-  header2.innerHTML = videolist[i][1];
-  divbody.appendChild(header2);
+        var divbody = document.createElement("div");
+        var att3 = document.createAttribute("class");
+        att3.value = "card-body";
+        divbody.setAttributeNode(att3);
 
-  var alink = document.createElement("a");
-  var att4 = document.createAttribute("href");
-  att4.value = videolist[i][2];
-  alink.setAttributeNode(att4);
-  alink.innerHTML = videolist[i][2];
-  divbody.appendChild(alink);
+        var header2 = document.createElement("h6");
+        var att3 = document.createAttribute("class");
+        att3.value = "card-title";
+        header2.setAttributeNode(att3);
+        header2.innerHTML = videolist[i][1];
+        divbody.appendChild(header2);
 
-  div.appendChild(divbody)
-  show.appendChild(div);
+        var alink = document.createElement("a");
+        var att4 = document.createAttribute("href");
+        att4.value = videolist[i][2];
+        alink.setAttributeNode(att4);
+        alink.innerHTML = videolist[i][2];
+        divbody.appendChild(alink);
+
+        div.appendChild(divbody)
+        show.appendChild(div);
+      }
+
+      var addlink = document.getElementsByTagName("a");
+
+      function clicklink(e){
+        // console.log(e);
+        chrome.tabs.create({url: e})
+      }
+      // console.log(addlink);
+      for(var i = 0; i< addlink.length; i++){
+        var e = addlink[i].innerHTML;
+        console.log(e);
+        addlink[i].addEventListener('click',clicklink.bind(this,e));
+      }
+      break;
+    case 2:
+      // group
+      var show = document.getElementById("showjs");
+      show.innerHTML = "";
+      var headtitle = document.createElement("h4");
+      headtitle.innerHTML = "Tech Channel:";
+      show.appendChild(headtitle);
+      var grouplist = [];
+
+      var groupmember = ["Linus Tech Tips","Austin Evans","Dave Lee","feverSound com","Jonathan Morrison","Lok Cheung HK","Marques Brownlee","Matthew Moniz","The Tech Chap"];
+      for(var i = 0; i < videolist.length; i++){
+        if(groupmember.includes(videolist[i][0])){
+          grouplist.push(videolist[i]);
+        }
+      }
+
+      for(var i = 0; i < 15; i++){
+        var div = document.createElement("div");
+        var att = document.createAttribute("class");
+        att.value = "card";
+        div.setAttributeNode(att);
+
+        var header = document.createElement("h5");
+        var att2 = document.createAttribute("class");
+        att2.value = "card-header";
+        header.setAttributeNode(att2);
+        header.innerHTML = grouplist[i][0];
+        div.appendChild(header);
+
+        var divbody = document.createElement("div");
+        var att3 = document.createAttribute("class");
+        att3.value = "card-body";
+        divbody.setAttributeNode(att3);
+
+        var header2 = document.createElement("h6");
+        var att3 = document.createAttribute("class");
+        att3.value = "card-title";
+        header2.setAttributeNode(att3);
+        header2.innerHTML = grouplist[i][1];
+        divbody.appendChild(header2);
+
+        var alink = document.createElement("a");
+        var att4 = document.createAttribute("href");
+        att4.value = grouplist[i][2];
+        alink.setAttributeNode(att4);
+        alink.innerHTML = grouplist[i][2];
+        divbody.appendChild(alink);
+
+        div.appendChild(divbody)
+        show.appendChild(div);
+      }
+
+      var addlink = document.getElementsByTagName("a");
+
+      function clicklink(e){
+        // console.log(e);
+        chrome.tabs.create({url: e})
+      }
+      // console.log(addlink);
+      for(var i = 0; i< addlink.length; i++){
+        var e = addlink[i].innerHTML;
+        console.log(e);
+        addlink[i].addEventListener('click',clicklink.bind(this,e));
+      }
+      break;
+    case 3:
+      // preference
+      var show = document.getElementById("showjs");
+      show.innerHTML = "";
+      var headtitle = document.createElement("h4");
+      chrome.storage.sync.get(['preference'], function(result) {
+        console.log('Value currently is ' + result.preference);
+        var preferencemember = result.preference;
+        headtitle.innerHTML = "Preference: " + preferencemember;
+        show.appendChild(headtitle);
+        var preferencelist = [];
+
+        for(var i = 0; i < 100; i++){
+          if(preferencemember === (videolist[i][0])){
+              preferencelist.push(videolist[i]);
+          }
+        }
+
+        for(var i = 0; i < 15; i++){
+          if(!preferencelist.includes(videolist[i])){
+            preferencelist.push(videolist[i]);
+          }
+        }
+
+        for(var i = 0; i < 15; i++){
+          var div = document.createElement("div");
+          var att = document.createAttribute("class");
+          att.value = "card";
+          div.setAttributeNode(att);
+
+          var header = document.createElement("h5");
+          var att2 = document.createAttribute("class");
+          att2.value = "card-header";
+          header.setAttributeNode(att2);
+          header.innerHTML = preferencelist[i][0];
+          div.appendChild(header);
+
+          var divbody = document.createElement("div");
+          var att3 = document.createAttribute("class");
+          att3.value = "card-body";
+          divbody.setAttributeNode(att3);
+
+          var header2 = document.createElement("h6");
+          var att3 = document.createAttribute("class");
+          att3.value = "card-title";
+          header2.setAttributeNode(att3);
+          header2.innerHTML = preferencelist[i][1];
+          divbody.appendChild(header2);
+
+          var alink = document.createElement("a");
+          var att4 = document.createAttribute("href");
+          att4.value = preferencelist[i][2];
+          alink.setAttributeNode(att4);
+          alink.innerHTML = preferencelist[i][2];
+          divbody.appendChild(alink);
+
+          div.appendChild(divbody)
+          show.appendChild(div);
+        }
+
+        var addlink = document.getElementsByTagName("a");
+
+        function clicklink(e){
+          // console.log(e);
+          chrome.tabs.create({url: e})
+        }
+        // console.log(addlink);
+        for(var i = 0; i< addlink.length; i++){
+          var e = addlink[i].innerHTML;
+          console.log(e);
+          addlink[i].addEventListener('click',clicklink.bind(this,e));
+        }
+      });
+
+      break;
+    case 4:
+      var show = document.getElementById("showjs");
+      show.innerHTML = "<h4>unsibscribe recommend</h4>";
+      var unsublist = [];
+      var goodlist = [];
+      var theday = new Date();
+      var thedaynum = theday.getTime();
+      theday = theday - 31536000000;
+      console.log(thedaynum)
+      for(var i = 0; i < videolist.length; i++){
+        if(!goodlist.includes(videolist[i][0]) && !unsublist.includes(videolist[i][0])){
+          if(videolist[i][3].getTime() > theday){
+            goodlist.push(videolist[i][0]);
+          }else{
+            unsublist.push(videolist[i][0]);
+          }
+        }
+      }
+      // for(var i = 0; i < channels_names.length; i++){
+      //   if(!goodlist.includes(channels_names[i]) && !unsublist.includes(channels_names[i])){
+      //     unsublist.push(channels_names[i]);
+      //   }
+      // }
+      // console.log(unsublist);
+      for(var i = 0; i < unsublist.length; i++){
+        for(var j = 0; j < channels_link.length; j++){
+          if(channels_link[j][0] == unsublist[i]){
+            console.log(unsublist[i]);
+            console.log(channels_link[j][1]);
+            var div = document.createElement("div");
+            var att = document.createAttribute("class");
+            att.value = "card";
+            div.setAttributeNode(att);
+
+            var header = document.createElement("h5");
+            var att2 = document.createAttribute("class");
+            att2.value = "card-header";
+            header.setAttributeNode(att2);
+            header.innerHTML = unsublist[i];
+            div.appendChild(header);
+
+            var divbody = document.createElement("div");
+            var att3 = document.createAttribute("class");
+            att3.value = "card-body";
+            divbody.setAttributeNode(att3);
+
+            var alink = document.createElement("a");
+            var att4 = document.createAttribute("href");
+            att4.value = channels_link[j][1];
+            alink.setAttributeNode(att4);
+            alink.innerHTML = channels_link[j][1];
+            divbody.appendChild(alink);
+
+            div.appendChild(divbody)
+            show.appendChild(div);
+            break;
+          }
+        }
+      }
+
+      var addlink = document.getElementsByTagName("a");
+
+      function clicklink(e){
+        // console.log(e);
+        chrome.tabs.create({url: e})
+      }
+      // console.log(addlink);
+      for(var i = 0; i< addlink.length; i++){
+        var e = addlink[i].innerHTML;
+        console.log(e);
+        addlink[i].addEventListener('click',clicklink.bind(this,e));
+      }
+
+      break;
+    case 5:
+      var show = document.getElementById("showjs");
+      show.innerHTML = "<h4>Setting</h4>";
+
+      chrome.storage.sync.get(['preference'], function(result) {
+          if(typeof result.preference === undefined){
+              chrome.storage.sync.set({'preference': ""}, function() {
+                console.log('Value is set to ' + "null");
+              });
+          }
+          var preference_text = result.preference;
+
+          function update() {
+            preference = document.getElementById("preferencetext").value;
+            if (!preference) {
+              console.log('error');
+              return;
+            }
+            chrome.storage.sync.set({'preference': preference}, function() {
+              console.log('Value is set to ' + preference);
+            });
+            chrome.storage.sync.get(['preference'], function(result) {
+              console.log('Value currently is ' + result.preference);
+            });
+          }
+
+          var input = document.createElement("input");
+          input.setAttribute('type', 'text');
+          input.setAttribute('id', 'preferencetext');
+          input.setAttribute("value", preference_text);
+          show.appendChild(input);
+
+          var button = document.createElement("button");
+          button.innerHTML = "Save";
+          button.setAttribute("id", "savebutton");
+          button.addEventListener("click", update);
+          show.appendChild(button);
+
+
+      });
+
+      break;
+  }
 }
 
-var addlink = document.getElementsByTagName("a");
-
-
-function clicklink(e){
-  // console.log(e);
-  chrome.tabs.create({url: e})
-}
-// console.log(addlink);
-for(var i = 0; i< addlink.length; i++){
-  var e = addlink[i].innerHTML;
-  console.log(e);
-  addlink[i].addEventListener('click',clicklink.bind(this,e));
-}
+var defaultbutton = document.getElementById("default");
+var groupbutton = document.getElementById("group");
+var preferencebutton = document.getElementById("preference");
+var unsubbutton = document.getElementById("unsub");
+var settingbutton = document.getElementById("setting");
+// console.log(defaultbutton);
+// console.log(groupbutton);
+// console.log(preferencebutton);
+// console.log(unsubbutton);
+defaultbutton.addEventListener('click',clickpage.bind(this,1));
+groupbutton.addEventListener('click',clickpage.bind(this,2));
+preferencebutton.addEventListener('click',clickpage.bind(this,3));
+unsubbutton.addEventListener('click',clickpage.bind(this,4));
+setting.addEventListener('click',clickpage.bind(this,5));
